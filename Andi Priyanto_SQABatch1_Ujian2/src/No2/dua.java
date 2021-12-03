44f080cbb0428ca39f9751cb82329fe9aeaad890
Andi Priyanto <andi_r45@yahoo.com>
2021-12-03 17:49:16 +0700

    next

---
+++ Andi Priyanto_SQABatch1_Ujian2/src/No2/dua.java
@@ -0,0 +1,54 @@
+package No2;
+
+import java.util.Scanner;
+public class dua {
+
+	
+	public static void main(String[]args) {
+		Scanner in = new Scanner (System.in);
+		System.out.println("Masukkan merk kendaraan = ");
+		String merk = in.nextLine();
+		System.out.println("Masukkan tipe kendaraan = ");
+		String tipe = in.nextLine();
+		System.out.println("Masukkan tahun pembuatan = ");
+		int tahun = in.nextInt();
+	
+		int pajak = hitungPajak (tahun);
+		System.out.println("Pajak tahunan untuk "+merk+" "+tipe+" tahun "+tahun+ " adalah Rp. "+pajak);
+		
+	
+	}
+
+	
+
+	 public static int hitungPajak(int produksi) {
+
+
+		 int pajak;
+			if (produksi>2020) {
+			pajak = 4000000;
+			}else if (produksi>2010) {
+			pajak = 2000000;
+			}else if(produksi>2000){
+			pajak = 1000000;
+			}else if(produksi>1990) {
+			pajak = 750000;
+			}else if(produksi>1980) {
+			pajak = 500000;
+			}else {
+			pajak = 250000;
+			}
+			return pajak;
+		
+			
+
+	
+	}
+
+
+
+	}
+
+
+	
+
